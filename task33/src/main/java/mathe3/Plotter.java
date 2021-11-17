0d13f2d74ff4095a56345329ef1bd875df76ede9
Denys Chernyshov <den.blackshov@gmail.com>
2021-11-17 21:11:47 +0100

    Clean plotter

--- task33/src/main/java/mathe3/Plotter.java
+++ task33/src/main/java/mathe3/Plotter.java
@@ -4,82 +4,88 @@ import java.awt.Color;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 import java.util.function.DoubleUnaryOperator;
 import java.util.stream.Collectors;
 
 public class Plotter {
 
-    private final int nPoints;
+    private static final double padding = 0.5;
+    private static final int HEIGHT = 768;
+    private static final int WIDTH = 768;
+    private final Random random = new Random();
+
     private final Plot plot;
-    private final double xStep;
-    private int plotNumber= 0;
-    private Color[] colors = new Color[]{Color.BLACK, Color.CYAN, Color.GREEN, Color.MAGENTA, Color.YELLOW, Color.WHITE};
-    private double padding = 0.5;
+    private final double step;
+
     public Plotter(List<Point> points, int nPoints) {
+        double[] minMax = calculateBounds(points);
+        double minX = minMax[0] - padding;
+        double maxX = minMax[1] + padding;
+        double minY = minMax[2] - padding;
+        double maxY = minMax[3] + padding;
+        step = (maxX - minX) / nPoints;
+        plot = Plot
+                .plot(Plot.plotOpts().height(HEIGHT).width(WIDTH))
+                .xAxis("x", Plot.axisOpts().range(minX, maxX))
+                .yAxis("y", Plot.axisOpts().range(minY, maxY))
+                .series(randomName(), Plot.data()
+                        .xy(0, minY)
+                        .xy(0, maxY), Plot.seriesOpts().line(Plot.Line.DASHED).color(Color.BLACK))
+                .series(randomName(), Plot.data()
+                        .xy(minX, 0)
+                        .xy(maxX, 0), Plot.seriesOpts().line(Plot.Line.DASHED).color(Color.BLACK));
+    }
+
+    private String randomName() {
+        return UUID.randomUUID().toString();
+    }
+
+    private Color randomColor(){
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255));
+    }
+
+    private double[] calculateBounds(List<Point> points) {
         double xMin = 0;
         double xMax = 0;
         double yMin = 0;
         double yMax = 0;
-        for (Point point : points){
-            if (point.x > xMax){
-                xMax = point.x;
-            }
-            if (point.x < xMin){
-                xMin = point.x;
-            }
-            if (point.y < yMin){
-                yMin = point.y;
-            }
-            if (point.y > yMax){
-                yMax = point.y;
-            }
+        for (Point point : points) {
+            if (point.x > xMax) xMax = point.x;
+            if (point.x < xMin) xMin = point.x;
+            if (point.y < yMin) yMin = point.y;
+            if (point.y > yMax) yMax = point.y;
         }
         double maxDiff = Math.max(xMax - xMin, yMax - yMin);
-        double minX = xMin - padding;
-        double maxX = xMin + maxDiff + padding;
-        double minY = yMin - padding;
-        double maxY = yMin + maxDiff + padding;
-
-        this.nPoints = nPoints;
-        xStep = (maxX - minX) / this.nPoints;
-        plot = Plot.plot(Plot.plotOpts().height(1024).width(1024))
-        .xAxis("x", Plot.axisOpts().range(minX, maxX))
-        .yAxis("y", Plot.axisOpts().range(minY, maxY))
-        .series(UUID.randomUUID().toString(), Plot.data()
-                .xy(0, minY)
-                .xy(0, maxY), Plot.seriesOpts().line(Plot.Line.DASHED).color(Color.BLACK))
-        .series(UUID.randomUUID().toString(), Plot.data()
-                .xy(minX,0)
-                .xy(maxX, 0), Plot.seriesOpts().line(Plot.Line.DASHED).color(Color.BLACK));
+        return new double[]{xMin, xMin + maxDiff, yMin, yMin + maxDiff};
     }
 
     public Plotter function(DoubleUnaryOperator fx, double leftbound, double rightbound) {
         List<Double> xs = new ArrayList<>();
         List<Double> ys = new ArrayList<>();
-        for (double x = leftbound; x <= rightbound; x += xStep) {
+        for (double x = leftbound; x <= rightbound; x += step) {
             xs.add(x);
             ys.add(fx.applyAsDouble(x));
         }
-        Plot.Data data = Plot.data()
-                .xy(xs, ys);
+        Plot.Data data = Plot.data().xy(xs, ys);
 
-        plot.series(UUID.randomUUID().toString(), data, Plot.seriesOpts().color(colors[plotNumber++]));
+        plot.series(randomName(), data, Plot.seriesOpts().color(randomColor()));
         return this;
     }
 
-    public Plotter point(Point point){
+    public Plotter point(Point point) {
         return points(List.of(point));
     }
 
-    public Plotter points(List<Point> points){
+    public Plotter points(List<Point> points) {
         List<Double> xs = points.stream().map(p -> p.x).collect(Collectors.toList());
         List<Double> ys = points.stream().map(p -> p.y).collect(Collectors.toList());
-        plot.series(UUID.randomUUID().toString(),
+        plot.series(randomName(),
                 Plot.data().xy(xs, ys),
                 Plot.seriesOpts().markerColor(Color.GREEN)
-        .marker(Plot.Marker.CIRCLE)
-        .line(Plot.Line.NONE));
+                        .marker(Plot.Marker.CIRCLE)
+                        .line(Plot.Line.NONE));
         return this;
     }
 
