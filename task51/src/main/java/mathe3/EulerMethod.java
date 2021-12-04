499b8997e6b3fa6827c1a8ce9484e838ac03dcca
Denys Chernyshov <den.blackshov@gmail.com>
2021-12-04 14:49:21 +0100

    implement heun method

--- task51/src/main/java/mathe3/EulerMethod.java
+++ task51/src/main/java/mathe3/EulerMethod.java
@@ -6,10 +6,6 @@ import java.util.function.DoubleBinaryOperator;
 import mathe3.plot.Point;
 
 public class EulerMethod {
-
-    public EulerMethod() {
-    }
-
     public List<Point> calculatePoints(DoubleBinaryOperator diffFunction, Point condition, double from, double to, double h){
         List<Point> points = new ArrayList<>();
         points.add(condition);
@@ -17,17 +13,14 @@ public class EulerMethod {
         Point prevPoint = condition;
         int n = (int) ((from - to)/h); // number of points
         for(int i = 1; i <= n; i++){
-            double slope = diffFunction.applyAsDouble(prevPoint.x, prevPoint.y);
-            Point nextPoint = addVectorToPoint(prevPoint, h, h * slope);
+            double slope = diffFunction.applyAsDouble(prevPoint.x, prevPoint.y); // f(xi, yi)
 
-            prevPoint = nextPoint;
-            points.add(nextPoint);
+            double x = prevPoint.x + h;         // x for i+1
+            double y = prevPoint.y + h * slope; // y for i + 1
+
+            prevPoint = new Point(x, y);
+            points.add(prevPoint);
         }
         return points;
     }
-
-    private Point addVectorToPoint(Point original, double vectorX, double vectorY){
-        return new Point(original.x + vectorX, original.y + vectorY);
-    }
-
 }
---
+++ task51/src/main/java/mathe3/HeunMethod.java
@@ -0,0 +1,34 @@
+package mathe3;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.DoubleBinaryOperator;
+import mathe3.plot.Point;
+
+public class HeunMethod {
+    public List<Point> calculatePoints(DoubleBinaryOperator diffFunction, Point condition, double from, double to, double h){
+        List<Point> points = new ArrayList<>();
+        points.add(condition);
+
+        Point prevPoint = condition;
+        int n = (int) ((from - to)/h); // number of points
+        for(int i = 1; i <= n; i++){
+            double prevX = prevPoint.x; // x for i
+            double prevY = prevPoint.y; // y for i
+
+            double x = prevX + h;       // x for i+1
+
+            double slope = diffFunction.applyAsDouble(prevX, prevY); // f(xi, yi)
+            double predictor = prevY + h * slope;  // predictor `y` for i+1
+
+            double predictorSlope = diffFunction.applyAsDouble(x, predictor); // f(x(i+1), `y`(i+1))
+            double corrector = prevY + 0.5 * h * slope + 0.5 * h * predictorSlope;    // corrector y for i+1
+
+            prevPoint = new Point(x, corrector);
+            points.add(prevPoint);
+        }
+
+        return points;
+    }
+
+}
