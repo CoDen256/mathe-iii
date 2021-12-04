4922e70039ba166ed03fd0c6d2dea678b04b1a19
Denys Chernyshov <den.blackshov@gmail.com>
2021-12-04 15:04:43 +0100

    Add functions

--- task51/src/main/java/mathe3/Task51ABC.java
+++ task51/src/main/java/mathe3/Task51ABC.java
@@ -1,7 +1,32 @@
 package mathe3;
 
+import java.util.List;
+import java.util.Map;
+import java.util.function.DoubleUnaryOperator;
+import java.util.stream.Collectors;
+import mathe3.plot.Point;
+
 public class Task51ABC {
+
     public static void main(String[] args) {
+
+
+
+    }
+
+
+    // actual y(x)
+    public static double y(double x){
+        return 89 / 218. * Math.exp(-3. / 2 * x) - 3 / 109. * Math.sin(5 * x) + 10 / 109. * Math.cos(5 * x);
+    }
+
+    // y'(x)
+    public static double yDerivative(double x, double y){
+        return -3. / 2 * y - 0.5 * Math.sin(x);
+    }
+
+    public static Map<Double, Double> convertPointsToMap(List<Point> pointList){
+        return pointList.stream().collect(Collectors.toMap(Point::getX, Point::getY));
     }
 
 }
--- task51/src/main/java/mathe3/Task51D.java
+++ task51/src/main/java/mathe3/Task51D.java
@@ -5,4 +5,10 @@ public class Task51D {
     public static void main(String[] args) {
 
     }
+
+
+    // y'(x)
+    public static double yDerivative(double x, double y){
+        return y - Math.cos(2*x);
+    }
 }
